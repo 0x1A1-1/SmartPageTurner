ddc94edb9aa6a036a2c9fbd7dccd8bf696195573
Cigarent <xiaojunh1996@hotmail.com>
2018-02-25 11:54:13 -0600

    Add comment for the code

--- OpenCV-py/landmark_with_center.py
+++ OpenCV-py/landmark_with_center.py
@@ -1,17 +1,14 @@
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils import face_utils
-import datetime
 import time
 import argparse
 import imutils
 import dlib
 import cv2
 import eye_local_fabian
-from scipy import ndimage
 import numpy as np
 from scipy.spatial import distance as dist
-from pprint import pprint
 import requests
 
 # construct the argument parser and parse the arguments
@@ -22,14 +19,16 @@ ap.add_argument("-r", "--camera", type=int, default=-1,
 	help="which camera should be used")
 args = vars(ap.parse_args())
 
+# Global vars
 FRAME_THRESHOLD = 10
-RATIO_THRESHOLD_LB = 0.40
-RATIO_THRESHOLD_UB = 0.60
+RATIO_THRESHOLD_LB = 0.45
+RATIO_THRESHOLD_UB = 0.55
 COUNTER = 0
 TRIGGERED = 0
 MAX = 0
 MIN = 100
 
+# Draw boxes for eyes
 def get_eye_box(eye):
 
     #print(eye)
@@ -42,6 +41,7 @@ def get_eye_box(eye):
 
     return (left, top, width, height)
 
+# Determine the center of the eyes 
 def eye_center(frame_grey, eyes):
     # x,y,width,height = face
     # faceROI = frame_grey[y:y+height, x:x+width]
@@ -105,6 +105,7 @@ def eye_center(frame_grey, eyes):
 
     return (0,0)
 
+# Calculate GCR for detecting rapid change
 def gaze_corner_ratio(eye, center):
 
     bottom_right_dist = dist.euclidean(eye[0], center) + dist.euclidean(eye[5], center)
@@ -112,6 +113,7 @@ def gaze_corner_ratio(eye, center):
 
     return bottom_right_dist/top_left_dist
 
+# Get the ratio of eye at the left bottom of the screen ( bottom right of real eye )
 def bottom_left_region(eye, center):
 
     x_ratio  = (center[0] - eye[0][0])/(eye[3][0] - eye[0][0])
@@ -130,10 +132,9 @@ eye_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_
 print("[INFO] camera sensor warming up...")
 # print(args["camera"])
 vs = VideoStream(args["camera"]).start()
-vs.stream.set(cv2.CAP_PROP_FPS, 20)
-# pprint(vs.stream)
 time.sleep(2.0)
 
+# Get the index of facial landmarks
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
@@ -157,38 +158,35 @@ while True:
         # array
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-        # if len(rect)>1:
-        #     (rectx, recty) = rect[0]
-        #     (rectw, recth) = rect[1]
 
+        # Get left and right eyes
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
 
+        # generate boxes for left/right eyes
         (lx, ly, lw, lh) = get_eye_box(leftEye)
         (rx, ry, rw, rh) = get_eye_box(rightEye)
-
         eyeboxes = [(lx, ly, lw, lh), (rx, ry, rw, rh)]
 
+        # Draw rectangle for eyes and face for testing purpose
         cv2.rectangle(frame, (lx, ly), (lx + lw, ly + lh), (0, 255, 255), 2)
         cv2.rectangle(frame, (rx, ry), (rx + rw, ry + rh), (255, 255, 0), 2)
         cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (255, 0, 0), 2)
         #detected = eye_cascade.detectMultiScale(gray, 1.3, 5)
 
+        # Determine and draw the center of the left eye
         (lcenterx, lcentery) = eye_center(gray, eyeboxes)
-
         cv2.circle(frame, (lcenterx, lcentery), 1, (255, 255, 255), -1)
 
+        # gcr is one of the implementation that detects more on rate of change rather than absolute value
         #gcr_result = gaze_corner_ratio(leftEye, (lcenterx, lcentery))
+
+        # Calculate absolute ratio for left eye center
         (xratio, yratio) = bottom_left_region(leftEye, (lcenterx, lcentery))
 
-        cv2.putText(frame, "X%: {}".format(xratio), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "Y%: {}".format(yratio), (10, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
+        # Count the center of X in threshold
         if RATIO_THRESHOLD_LB<xratio<RATIO_THRESHOLD_UB:
             COUNTER += 1
-
         else:
             if COUNTER >= FRAME_THRESHOLD:
                 TRIGGERED += 1
@@ -196,12 +194,18 @@ while True:
                 # reset the eye frame counter
                 COUNTER = 0
 
-        # print(leftEye[5][1] - leftEye[1][1])
+        # Calculate the MIN and MAX eye height can possibly be for testing
         if (leftEye[5][1] - leftEye[1][1])>MAX:
             MAX = leftEye[5][1] - leftEye[1][1]
 
         if (leftEye[5][1] - leftEye[1][1])<MIN:
             MIN = leftEye[5][1] - leftEye[1][1]
+
+        # Output texts
+        cv2.putText(frame, "X%: {}".format(xratio), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Y%: {}".format(yratio), (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
         cv2.putText(frame, "TRIG: {}".format(TRIGGERED), (10, 90),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
@@ -216,7 +220,7 @@ while True:
             cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
 
-    # show the frame
+    # show the frame for testing
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
@@ -224,20 +228,21 @@ while True:
     if key == ord("q"):
         break
 
-    if TRIGGERED >= 2:
-        print("POST SENT")
-        resp = requests.post("http://172.16.10.9:3000/flip", json={"flip":True} )
-        pprint(resp)
-        # if (resp.confirm == True):
-        resp = requests.get("http://172.16.10.9:3000/flip")
-        while resp.json() != True:
-            pprint(resp.text)
-            pprint(resp.content)
-            pprint(resp.json())
-            resp = requests.get("http://172.16.10.9:3000/flip")
-            input("heyya")
-            continue
-        TRIGGERED = 0
+    # Uncomment this line for localhost testing
+    # if TRIGGERED >= 2:
+    #     print("POST SENT")
+    #     resp = requests.post("http://172.16.46.128:3000/flip", json={"flip":True} )
+    #     pprint(resp)
+    #     # if (resp.confirm == True):
+    #     resp = requests.get("http://172.16.46.128:3000/flip")
+    #     while resp.json() != True:
+    #         pprint(resp.text)
+    #         pprint(resp.content)
+    #         pprint(resp.json())
+    #         resp = requests.get("http://172.16.46.128:3000/flip")
+    #         input("heyya")
+    #         continue
+    #     TRIGGERED = 0
 
 # do a bit of cleanup
 cv2.destroyAllWindows()

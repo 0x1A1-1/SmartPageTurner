4fd25ffa62728cb9fa44e9aac27398e1a3a687dc
Cigarent <xiaojunh1996@hotmail.com>
2018-04-28 09:55:24 -0500

    update trigger

--- Voice-Trigger/livespeech.py
+++ Voice-Trigger/livespeech.py
@@ -9,6 +9,11 @@
 
 from pocketsphinx import LiveSpeech
 #speech = LiveSpeech(lm=False, keyphrase='forward', kws_threshold=1e+20)
-speech = LiveSpeech()
+speech = LiveSpeech(buffer_size=256)
 for phrase in speech:
     print(phrase.segments(detailed=True))
+    #print(phrase.segments())
+    if "next" in phrase.segments() and "page" in phrase.segments():
+        print("NEXT")
+    elif ("previous" or "last") and "page" in phrase.segments():
+        print("PREV")
